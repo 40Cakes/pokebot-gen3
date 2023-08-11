6eae29b8137c19b588a37095095622e52e617b62
40 Cakes <16377135+40Cakes@users.noreply.github.com>
2023-08-11 11:05:17 +1000

    Add simple framecount perf test

--- perf.py
+++ perf.py
@@ -3,5 +3,6 @@ from modules.Memory import GetFrameCount
 while True:
     start = GetFrameCount()
     while GetFrameCount() == start:
+        #print(GetFrameCount())
         pass
     if GetFrameCount() > start + 1: print("Missed a frame!")
