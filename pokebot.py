020f5a467d9bcfea3ab28c54b1edb4a068ad823e
Spile <chickencraft12@gmail.com>
2023-08-23 11:02:29 +0100

    Added Emerald starter resets

--- pokebot.py
+++ pokebot.py
@@ -14,9 +14,17 @@ try:
         case 'starter':
             print(GetGame())
             if GetGame() == 'Pokémon Emerald':
+                if config['starter'] != ('mudkip' or 'treecko' or 'torchic'):
+                    print('Incompatable starter')
+                    input('Press enter to exit...')
+                    os._exit(1)
                 from modules.gen3.rse.General import Starter
                 Starter(config['starter'])
             elif GetGame() == 'Pokémon LeafGreen' or GetGame() == 'Pokémon FireRed':
+                if config['starter'] != ('bulbasaur' or 'charmander' or 'squirtle'):
+                    print('Incompatable starter')
+                    input('Press enter to exit...')
+                    os._exit(1)
                 from modules.gen3.rse.General import FRLGStarter
                 FRLGStarter(config['starter'])
             else:
