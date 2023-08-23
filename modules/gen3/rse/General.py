8ec1535b76d49bda2549b10ce6b68fb0432bf0c4
Spilare <126873007+Spilare@users.noreply.github.com>
2023-08-23 08:42:43 +0100

    Update General.py

--- modules/gen3/rse/General.py
+++ modules/gen3/rse/General.py
@@ -1,7 +1,7 @@
 import random
 from modules.Console import console
 from modules.Inputs import PressButton, WaitFrames
-from modules.Memory import GetTrainer, GetOpponent, OpponentChanged, TrainerState
+from modules.Memory import GetTrainer, GetOpponent, OpponentChanged, TrainerState, ReadSymbol
 from modules.Stats import EncounterPokemon
 
 
@@ -16,5 +16,45 @@ def ModeSpin():
             directions.remove(GetTrainer()['facing'])
             PressButton([random.choice(directions)])
             WaitFrames(6)
+    except Exception:
+        console.print_exception()
+
+def Starter(Choice: str):
+    try:
+        ListOfRngSeeds = []
+        while True:
+            x = 0
+            # i = i + 1
+            while ReadSymbol('sStarterLabelWindowId') != b'\x01\x00':
+                PressButton((['A'],10))
+            while ReadSymbol('sStarterLabelWindowId') == b'\xFF\x00':
+                PressButton((['B'],10))
+            while ReadSymbol('sStarterLabelWindowId') == b'\x01\x00':
+                PressButton((['Right'],10))
+                PressButton((['A'],10))
+            while ReadSymbol('gTasks', size = 1) != b'\x01':
+                None
+            while x != 1:
+                RNG = ReadSymbol('gRngValue', size = 4)
+                if RNG not in ListOfRngSeeds:
+                    PressButton((['A'], 1))
+                    ListOfRngSeeds.append(RNG)
+                    log.info(ReadSymbol('gRngValue', size = 4))
+
+                    while ReadSymbol('gTasks', size = 1) != b'\x0D':
+                        PressButton((['A'],1))
+                        x = 1
+            while ReadSymbol('gDisplayedStringBattle', size=4) != b'\xd1\xdc\xd5\xe8':
+                PressButton((['B'], 1))
+            if(GetParty()[0]['shiny']):
+                log.info('Shiny found!')
+                input('Press enter to continue...')
+                os._exit(0)
+            if(GetOpponent()['shiny']):
+                log.info('LOL Shiny zoooooooon')
+                input('Sadly Press Enter to Continue........')
+                os._exit(0)
+            while ReadSymbol('sStarterLabelWindowId') != b'\x00\x00':
+                PressButton((['A', 'B', 'Start', 'Select'], 1))
     except Exception:
         console.print_exception()
