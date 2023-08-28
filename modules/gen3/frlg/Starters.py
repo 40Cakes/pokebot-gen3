d0b2259854f5019b8744d2140bcf99468f3b2160
40 Cakes <16377135+40Cakes@users.noreply.github.com>
2023-08-28 20:39:16 +1000

    FRLG starter fix

--- modules/gen3/frlg/Starters.py
+++ modules/gen3/frlg/Starters.py
@@ -2,6 +2,7 @@ import struct
 from modules.Console import console
 from modules.Inputs import PressButton
 from modules.Memory import GetTrainer, ReadSymbol, GetParty
+from modules.Navigation import FollowPath
 from modules.Stats import GetRNGStateHistory, SaveRNGStateHistory, EncounterPokemon
 
 
@@ -9,27 +10,26 @@ def Starters(choice: str):
     try:
         RNGStateHistory = GetRNGStateHistory(GetTrainer()['tid'], choice)
         while True:
-            Out = 0
             RNG = int(struct.unpack('<I', ReadSymbol('gRngValue', size=4))[0])
             while RNG in RNGStateHistory['rng']:
                 RNG = int(struct.unpack('<I', ReadSymbol('gRngValue', size=4))[0])
             RNGStateHistory['rng'].append(RNG)
             SaveRNGStateHistory(GetTrainer()['tid'], choice, RNGStateHistory)
+
             while ReadSymbol('gStringVar4', size=4) != b'\xbe\xe3\x00\xed':
-                PressButton(['A'], 10)
+                PressButton(['A'])
+
             while GetTrainer()['facing'] != 'Down':
-                PressButton(['B'], 10)
-                PressButton(['Down'], 10)
-            i = 0
-            while i < 5:
-                PressButton(['Down'],10)
-                i = i + 1
-            while Out == 0:
-                if ReadSymbol('gDisplayedStringBattle', size=4) == b'\xc9\xbb\xc5\xf0':
-                    Out = 1
-                PressButton(['Left'],10)
-                PressButton(['Down'],10)
-                PressButton(['B'],10)
+                PressButton(['B', 'Down'])
+
+            FollowPath([
+                (8, 7),
+                (7, 7),
+                (7, 8)
+            ])
+
+            while GetTrainer()['state'] != 3:
+                PressButton(['B'])
 
             EncounterPokemon(GetParty()[0])
             while ReadSymbol('gDisplayedStringBattle', size=4) != b'\x00\x00\x00\x00':
