5b6b3dfb1c9675c1f28cfad92c36c52deae55958
Teras <35451366+Terasol@users.noreply.github.com>
2023-09-07 17:46:04 +0200

    Add mapping from address to Symbol name
    makes it possible to get a symbol based on its address

--- modules/Memory.py
+++ modules/Memory.py
@@ -91,6 +91,18 @@ class emulator:
         else:
             self.symbols = None
 
+    def __addressymbolmap(self):
+        if self.sym_file:
+            self.addressymbolmap = {}
+            for s in open(f'modules/data/symbols/{self.sym_file}').readlines():
+                self.addressymbolmap[hex(int(s.split(' ')[0].strip(), 16))] = {
+                    'name': s.split(' ')[3].strip(),
+                    'type': str(s.split(' ')[1]),
+                    'size': int(s.split(' ')[2], 16)
+                }
+        else:
+            self.symbols = None
+
     def __init__(self, pid):
         self.proc = Pymem(pid)
         self.p_EWRAM = GetPointer(self.proc, self.proc.base_address + 0x02849A28,
@@ -106,6 +118,7 @@ class emulator:
         self.game_code = self.proc.read_bytes(self.p_ROM + 0xAC, 4).decode('utf-8')
         self.__game()
         self.__symbols()
+        self.__addressymbolmap()
 
 
 while True:
