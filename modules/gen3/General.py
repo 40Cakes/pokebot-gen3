5f569b2392d147212d7ec9ec2430494f1b8636ac
Eli <Mowlawnergame@gmail.com>
2023-09-08 22:24:44 -0400

    updated ModeSpin to properly encounter pokemon

--- modules/gen3/General.py
+++ modules/gen3/General.py
@@ -1,17 +1,23 @@
 import random
 from modules.Console import console
 from modules.Inputs import PressButton, WaitFrames, ReleaseInputs
-from modules.Memory import GetTrainer
+from modules.Memory import GetTrainer, OpponentChanged, GetOpponent
+from modules.Stats import EncounterPokemon
 
 
 def ModeSpin():
     try:
         # TODO home position, FollowPath() if trainer walks off
-        ReleaseInputs()
-        directions = ['Up', 'Right', 'Down', 'Left']
-        directions.remove(GetTrainer()['facing'])  # Remove currently facing direction from possible inputs
-        PressButton([random.choice(directions)])
-        WaitFrames(5)
+        while True:
+            if OpponentChanged():
+                while GetTrainer()['map'] != (0, 0):
+                    continue
+                EncounterPokemon(GetOpponent())
+            ReleaseInputs()
+            directions = ['Up', 'Right', 'Down', 'Left']
+            directions.remove(GetTrainer()['facing'])
+            PressButton([random.choice(directions)])
+            WaitFrames(6)
 
     except:
         console.print_exception(show_locals=True)
