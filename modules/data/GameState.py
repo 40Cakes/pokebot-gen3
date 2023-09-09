371d4f1881e3af2a4c88ec5368e94e8c86057e30
Eli <Mowlawnergame@gmail.com>
2023-09-08 22:24:13 -0400

    changed GameState to use symbols that are pointed at by gMain.callback2 and changed from IntEnum to StrEnum

--- modules/data/GameState.py
+++ modules/data/GameState.py
@@ -1,11 +1,19 @@
-from enum import IntEnum  # https://docs.python.org/3/library/enum.html
+from enum import StrEnum  # https://docs.python.org/3/library/enum.html
 
 
-class GameState(IntEnum):
-    OVERWORLD = 80
-    MISC_MENU = 255
-    BAG_MENU = 0
+class GameState(StrEnum):
+    OVERWORLD = "CB2_Overworld"
+    MISC_MENU = "sMenuCBTemp"
+    BAG_MENU = "CB2_BagMenuRun"
+    PARTY_MENU = "CB2_UpdatePartyMenu"
+    POKEDEX = "CB2_PokedexScreen"
     # Needs further investigation; these values have multiple meanings
-    BATTLE = 3
+    BATTLE = "BattleMainCB2"
+    ENTERING_BATTLE = "CB2_OverworldBasic"
+    BATTLE_START = "CB2_HandleStartBattle"
+    WHITEOUT = "CB2_WhiteOut"
+    EVOLUTION = "CB2_EvolutionSceneUpdate"
     # BATTLE_2 = 2
     # FOE_DEFEATED = 5
+    GARBAGE = "_"
+    GARBAGE_COLLECTION = ".gcc2_compiled."
