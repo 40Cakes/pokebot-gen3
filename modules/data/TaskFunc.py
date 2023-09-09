9c4571a4db29569cdafbb5c144701b0c54f64a2f
Eli <Mowlawnergame@gmail.com>
2023-09-08 22:20:55 -0400

    added start menu state to placeholder TaskFunc enum

--- modules/data/TaskFunc.py
+++ modules/data/TaskFunc.py
@@ -3,3 +3,4 @@ from enum import StrEnum  # https://docs.python.org/3/library/enum.html
 
 class TaskFunc(StrEnum):
     LEARN_MOVE = "Task_HandleReplaceMoveInput"
+    START_MENU = "Task_ShowStartMenu"
