ea4ae65b48672ff69786614d9c1dd871350a3225
Eli <Mowlawnergame@gmail.com>
2023-09-13 06:25:37 -0400

    fleshed out move learning, start menu, and party menu task functions for taskfunc enum

--- modules/data/TaskFunc.py
+++ modules/data/TaskFunc.py
@@ -2,5 +2,10 @@ from enum import StrEnum  # https://docs.python.org/3/library/enum.html
 
 
 class TaskFunc(StrEnum):
-    LEARN_MOVE = "Task_HandleReplaceMoveInput"
-    START_MENU = "Task_ShowStartMenu"
+    LEARN_MOVE_RS = "sub_809E260"
+    LEARN_MOVE_E = "Task_HandleReplaceMoveInput"
+    LEARN_MOVE_FRLG = "Task_InputHandler_SelectOrForgetMove"
+    START_MENU_RS = "sub_80712B4"  # whoever decided on this name is not a good person
+    START_MENU_E = "Task_ShowStartMenu"
+    START_MENU_FRLG = "Task_StartMenuHandleInput"
+    PARTY_MENU_RS = "HandleDefaultPartyMenu"
