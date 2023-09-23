430c4d6995f292da7b6d72943a1d2f9ffc358a3c
40 Cakes <16377135+40Cakes@users.noreply.github.com>
2023-09-23 15:04:35 +1000

    Unfuck General.py

--- modules/modes/General.py
+++ modules/modes/General.py
@@ -2,17 +2,23 @@ import random
 from modules.Console import console
 from modules.Inputs import PressButton, WaitFrames, ReleaseInputs
 from modules.Memory import GetTask
+from modules.Navigation import FollowPath
 from modules.Trainer import GetTrainer
-from modules.CatchBlockList import GetBlockList
 
 
-blockList = GetBlockList()
+home = GetTrainer()['coords']
+
+
 def ModeSpin():
     try:
-        # TODO home position, FollowPath() if trainer walks off
+        if GetTrainer()['coords'] != home:
+            console.print('[red]Trainer has moved off the home tile! Attempting to walk back to {}...'.format(home))
+            FollowPath([(home[0], GetTrainer()['coords'][1])])
+            FollowPath([(GetTrainer()['coords'][0], home[1])])
+
         ReleaseInputs()
         directions = ['Up', 'Right', 'Down', 'Left']
-        directions.remove(GetTrainer()['facing'])  # Remove currently facing direction from possible inputs
+        directions.remove(GetTrainer()['facing'])
         PressButton([random.choice(directions)])
         WaitFrames(5)
 
