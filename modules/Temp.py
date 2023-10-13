8c023839f9e40f34f056dba3799f300f69fa5ba6
40 Cakes <16377135+40Cakes@users.noreply.github.com>
2023-10-13 11:54:40 +1100

    Quick safari zone fix (should work everywhere else too)

--- modules/Temp.py
+++ modules/Temp.py
@@ -1,16 +1,11 @@
 import struct
 
-from modules.Game import EncodeString
 from modules.Inputs import PressButton
-from modules.Memory import GetTask, ReadSymbol, GameState, GetGameState
+from modules.Memory import ReadSymbol, GameState, GetGameState
 
-b_What = EncodeString('What')  # TODO temporary fix (English only) for fishing in Safari Zone
 def temp_RunFromBattle():  # TODO temporary until auto-battle is fleshed out
-    while GetTask('TASK_PLAYCRYWHENRELEASEDFROMBALL') == {} and GetTask('SUB_8046AD0') == {} and \
-        ReadSymbol('gDisplayedStringBattle', size=4) != b_What:  # TODO temporary fix (English only) for fishing in Safari Zone
-        PressButton(['B'])
     while struct.unpack('<I', ReadSymbol('gActionSelectionCursor'))[0] != 1:
-        PressButton(['Right'])
+        PressButton(['B', 'Right'])
     while struct.unpack('<I', ReadSymbol('gActionSelectionCursor'))[0] != 3:
         PressButton(['Down'])
     while GetGameState() == GameState.BATTLE:
