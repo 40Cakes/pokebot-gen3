6d27a454e9336af86f2005f731fb1d8484e96349
40 Cakes <16377135+40Cakes@users.noreply.github.com>
2023-10-14 21:43:32 +1100

    Update to advance frame from loop instead of using WaitFrames()

--- modules/Navigation.py
+++ modules/Navigation.py
@@ -1,4 +1,5 @@
-from modules.Inputs import PressButton, WaitFrames, ReleaseInputs
+from modules.Config import config
+from modules.Gui import GetEmulator
 from modules.Memory import GetGameState, GameState
 from modules.Temp import temp_RunFromBattle
 from modules.Pokemon import OpponentChanged, GetOpponent
@@ -17,36 +18,37 @@ def FollowPath(coords: list, run: bool = True) -> bool:
     """
     for x, y, *map_data in coords:
         if run:
-            PressButton(['B'], 0)
+            GetEmulator().HoldButton("B")
 
-        while True:
+        while True and config["general"]["bot_mode"] != "manual":
             trainer = GetTrainer()
 
             if GetGameState() == GameState.BATTLE:
                 if OpponentChanged():
                     EncounterPokemon(GetOpponent())
+                GetEmulator().ReleaseButton("B")
                 temp_RunFromBattle()
 
             # Check if map changed to desired map
             if map_data:
-                if trainer['mapBank'] == map_data[0][0] and trainer['mapId'] == map_data[0][1]:
-                    ReleaseInputs()
+                if trainer["mapBank"] == map_data[0][0] and trainer["mapId"] == map_data[0][1]:
+                    GetEmulator().ReleaseButton("B")
                     break
 
-            if trainer['coords'][0] > x:
-                direction = 'Left'
-            elif trainer['coords'][0] < x:
-                direction = 'Right'
-            elif trainer['coords'][1] < y:
-                direction = 'Down'
-            elif trainer['coords'][1] > y:
-                direction = 'Up'
+            if trainer["coords"][0] > x:
+                direction = "Left"
+            elif trainer["coords"][0] < x:
+                direction = "Right"
+            elif trainer["coords"][1] < y:
+                direction = "Down"
+            elif trainer["coords"][1] > y:
+                direction = "Up"
             else:
-                ReleaseInputs()
+                GetEmulator().ReleaseButton("B")
                 break
 
-            PressButton([direction], 0)
-            WaitFrames(1)
+            GetEmulator().PressButton(direction)
+            GetEmulator().RunSingleFrame()  # TODO bad (needs to be refactored so main loop advances frame)
+        GetEmulator().RunSingleFrame()  # TODO bad (needs to be refactored so main loop advances frame)
 
-    ReleaseInputs()
     return True
--- modules/Temp.py
+++ modules/Temp.py
@@ -1,19 +1,21 @@
 import struct
 
-from modules.Game import EncodeString
-from modules.Inputs import PressButton
-from modules.Memory import GetTask, ReadSymbol, GameState, GetGameState
+from modules.Gui import GetEmulator
+from modules.Memory import ReadSymbol, GameState, GetGameState
 
-b_What = EncodeString('What')  # TODO temporary fix (English only) for fishing in Safari Zone
+
 def temp_RunFromBattle():  # TODO temporary until auto-battle is fleshed out
-    while GetTask('TASK_PLAYCRYWHENRELEASEDFROMBALL') == {} and GetTask('SUB_8046AD0') == {} and \
-        ReadSymbol('gDisplayedStringBattle', size=4) != b_What:  # TODO temporary fix (English only) for fishing in Safari Zone
-        PressButton(['B'])
     while struct.unpack('<I', ReadSymbol('gActionSelectionCursor'))[0] != 1:
-        PressButton(['Right'])
+        GetEmulator().PressButton('B')
+        GetEmulator().RunSingleFrame()  # TODO bad (needs to be refactored so main loop advances frame)
+        GetEmulator().PressButton('Right')
+        GetEmulator().RunSingleFrame()  # TODO bad (needs to be refactored so main loop advances frame)
     while struct.unpack('<I', ReadSymbol('gActionSelectionCursor'))[0] != 3:
-        PressButton(['Down'])
+        GetEmulator().PressButton('Down')
+        GetEmulator().RunSingleFrame()  # TODO bad (needs to be refactored so main loop advances frame)
     while GetGameState() == GameState.BATTLE:
-        PressButton(['A'])
+        GetEmulator().PressButton('A')
+        GetEmulator().RunSingleFrame()  # TODO bad (needs to be refactored so main loop advances frame)
     while GetGameState() != GameState.OVERWORLD:
-        PressButton(['B'])
+        GetEmulator().PressButton('B')
+        GetEmulator().RunSingleFrame()  # TODO bad (needs to be refactored so main loop advances frame)
