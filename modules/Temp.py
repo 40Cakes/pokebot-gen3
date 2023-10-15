897fd4cd058320eada157d8a9cf77647bdd33e29
40 Cakes <16377135+40Cakes@users.noreply.github.com>
2023-10-15 11:59:02 +1100

    Allow break out of temp_RunFromBattle() if switched to manual mode

--- modules/Temp.py
+++ modules/Temp.py
@@ -1,21 +1,26 @@
 import struct
 
+from modules.Config import config
 from modules.Gui import GetEmulator
 from modules.Memory import ReadSymbol, GameState, GetGameState
 
 
 def temp_RunFromBattle():  # TODO temporary until auto-battle is fleshed out
-    while struct.unpack('<I', ReadSymbol('gActionSelectionCursor'))[0] != 1:
+    while struct.unpack('<I', ReadSymbol('gActionSelectionCursor'))[0] != 1 and \
+            config['general']['bot_mode'] != 'manual':
         GetEmulator().PressButton('B')
         GetEmulator().RunSingleFrame()  # TODO bad (needs to be refactored so main loop advances frame)
         GetEmulator().PressButton('Right')
         GetEmulator().RunSingleFrame()  # TODO bad (needs to be refactored so main loop advances frame)
-    while struct.unpack('<I', ReadSymbol('gActionSelectionCursor'))[0] != 3:
+    while struct.unpack('<I', ReadSymbol('gActionSelectionCursor'))[0] != 3 and \
+            config['general']['bot_mode'] != 'manual':
         GetEmulator().PressButton('Down')
         GetEmulator().RunSingleFrame()  # TODO bad (needs to be refactored so main loop advances frame)
-    while GetGameState() == GameState.BATTLE:
+    while GetGameState() == GameState.BATTLE and \
+            config['general']['bot_mode'] != 'manual':
         GetEmulator().PressButton('A')
         GetEmulator().RunSingleFrame()  # TODO bad (needs to be refactored so main loop advances frame)
-    while GetGameState() != GameState.OVERWORLD:
+    while GetGameState() != GameState.OVERWORLD and \
+            config['general']['bot_mode'] != 'manual':
         GetEmulator().PressButton('B')
         GetEmulator().RunSingleFrame()  # TODO bad (needs to be refactored so main loop advances frame)
