5034a5f8ad448e80aeed0ec093bca6f505413c76
MelanX <info@melanx.de>
2023-10-25 19:13:57 +0200

    Set video output to true and speed to 1

--- modules/stats.py
+++ modules/stats.py
@@ -646,3 +646,5 @@ def encounter_pokemon(pokemon: Pokemon) -> None:
             get_emulator().create_save_state(suffix=filename_suffix)
 
             force_manual_mode()
+            get_emulator().set_speed_factor(1)
+            get_emulator().set_video_enabled(True)
