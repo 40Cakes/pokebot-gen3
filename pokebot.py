95c9044af31bdaf155241598ece7698aa4258b2b
40 Cakes <16377135+40Cakes@users.noreply.github.com>
2023-10-25 20:28:30 +1100

    Move tag/ver vars to top of file + linting

--- pokebot.py
+++ pokebot.py
@@ -3,13 +3,13 @@ import platform
 import sys
 from pathlib import Path
 
-from version import pokebot_name, pokebot_version
+from modules.version import pokebot_name, pokebot_version
 
-recommended_python_version = '3.11'
-supported_python_versions = [
-    (3, 10),
-    (3, 11)
-]
+recommended_python_version = "3.11"
+supported_python_versions = [(3, 10), (3, 11)]
+
+libmgba_tag = "0.2.0-2"
+libmgba_ver = "0.2.0"
 
 required_modules = [
     "numpy~=1.25.2",
@@ -25,12 +25,14 @@ required_modules = [
     "cffi~=1.15.1",
     "Pillow~=10.0.1",
     "sounddevice~=0.4.6",
-    "requests~=2.31.0"
+    "requests~=2.31.0",
 ]
 
 if platform.system() == "Windows":
-    required_modules.append("pywin32>=306")
-    required_modules.append("psutil~=5.9.5")
+    required_modules.extend([
+        "pywin32>=306",
+        "psutil~=5.9.5"
+    ])
 
 gui = None
 
@@ -41,16 +43,17 @@ gui = None
 # For those cases, we register an `atexit` handler that will wait for user input before closing
 # the terminal window.
 def on_exit() -> None:
-    if platform.system() == 'Windows':
+    if platform.system() == "Windows":
         import psutil
         import os
+
         parent_process_name = psutil.Process(os.getppid()).name()
-        if parent_process_name == 'py.exe':
+        if parent_process_name == "py.exe":
             if gui is not None and gui.window is not None:
                 gui.window.withdraw()
 
-            print('')
-            input('Press Enter to close...')
+            print("")
+            input("Press Enter to close...")
 
 
 atexit.register(on_exit)
@@ -62,10 +65,10 @@ def check_requirements() -> None:
     # We do not want to do downlaod requirements every single time the bot is started.
     # As a quick sanity check, we store the current bot version in `.last-requirements-check`.
     # If that file is present and contains the current bot version, we skip the check.
-    requirements_file = this_directory / '.last-requirements-check'
+    requirements_file = this_directory / ".last-requirements-check"
     need_to_fetch_requirements = True
     if requirements_file.is_file():
-        with open(requirements_file, 'r') as file:
+        with open(requirements_file, "r") as file:
             if file.read() == pokebot_version:
                 need_to_fetch_requirements = False
             else:
@@ -73,13 +76,13 @@ def check_requirements() -> None:
                     f"This is a newer version of {pokebot_name} than you have run before. "
                     f"We will have to check again if all requirements are met."
                 )
-                print('')
+                print("")
     else:
         print(
             f"Seems like this is the first time you are running {pokebot_name}!\n"
             "We will check if your system meets all the requirements to run it."
         )
-        print('')
+        print("")
 
     if need_to_fetch_requirements:
         python_version = platform.python_version_tuple()
@@ -89,7 +92,7 @@ def check_requirements() -> None:
                 version_matched = True
                 break
         if not version_matched:
-            supported_versions_list = ', '.join(map(lambda t: f'{str(t[0])}.{str(t[1])}', supported_python_versions))
+            supported_versions_list = ", ".join(map(lambda t: f"{str(t[0])}.{str(t[1])}", supported_python_versions))
             print(f"ERROR: The Python version you are using (Python {platform.python_version()}) is not supported.\n")
             print(f"Supported versions are: {supported_versions_list}")
             print(f"It is recommended that you install Python {recommended_python_version}.")
@@ -97,21 +100,21 @@ def check_requirements() -> None:
 
         # Some dependencies only work with 64-bit Python. Since this isn't the 90s anymore,
         # we'll just require that.
-        if platform.architecture()[0] != '64bit':
+        if platform.architecture()[0] != "64bit":
             print(f"ERROR: A 64-bit version of Python is required in order to run {pokebot_name} {pokebot_version}.\n")
             print(f"You are currently running a {platform.architecture()[0]} version.")
             sys.exit(1)
 
         # Run `pip install` on all required modules.
         import subprocess
+
         for module in required_modules:
-            subprocess.check_call([sys.executable, '-m', 'pip', 'install', module],
-                                  stderr=sys.stderr, stdout=sys.stdout)
+            subprocess.check_call(
+                [sys.executable, "-m", "pip", "install", module], stderr=sys.stderr, stdout=sys.stdout
+            )
 
         # Make sure that `libmgba-py` is installed.
-        print('')
-        libmgba_tag = "0.2.0-2"
-        libmgba_ver = "0.2.0"
+        print("")
         libmgba_directory = this_directory / "mgba"
         if not libmgba_directory.is_dir():
             match platform.system():
@@ -142,6 +145,7 @@ def check_requirements() -> None:
             import io
             import requests
             import zipfile
+
             response = requests.get(libmgba_url)
             if response.status_code == 200:
                 print("Unzipping libmgba into `./mgba`...")
@@ -150,10 +154,10 @@ def check_requirements() -> None:
 
         # Mark the requirements for the current bot version as checked, so we do not
         # have to run all of this again until the next update.
-        with open(requirements_file, 'w') as file:
+        with open(requirements_file, "w") as file:
             file.write(pokebot_version)
 
-    print('')
+    print("")
 
 
 if __name__ == "__main__":
@@ -162,7 +166,7 @@ if __name__ == "__main__":
     from modules.config import load_config_from_directory
     from modules.console import console
     from modules.gui import PokebotGui, get_emulator
-    from modules.main_loop import main_loop
+    from modules.main import main_loop
     from modules.profiles import profile_directory_exists, load_profile_by_name
 
     console.print(f"Starting [bold cyan]{pokebot_name} {pokebot_version}![/]")
@@ -174,13 +178,11 @@ if __name__ == "__main__":
     if platform.system() == "Windows":
         import win32api
 
-
         def win32_signal_handler(signal_type):
             if signal_type == 2:
                 emulator = get_emulator()
                 if emulator is not None:
                     emulator.shutdown()
-
 
         win32api.SetConsoleCtrlHandler(win32_signal_handler, True)
 
