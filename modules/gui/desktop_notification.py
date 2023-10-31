e197ba08e8a0628c4e1b75d76ea3022db85e68ad
Tino <1106400+hanzi@users.noreply.github.com>
2023-10-30 22:37:47 -0500

    Move import to top level in `desktop_notification.py`

--- modules/gui/desktop_notification.py
+++ modules/gui/desktop_notification.py
@@ -1,6 +1,5 @@
+import plyer
+
+
 def desktop_notification(title: str, message: str) -> None:
-    try:
-        import plyer
-        plyer.notification.notify(title=title, message=message)
-    except ImportError:
-        pass
+    plyer.notification.notify(title=title, message=message)
