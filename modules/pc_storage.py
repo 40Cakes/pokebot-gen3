de043315b4daefddbca9cb003da0bab8874918be
Tino <1106400+hanzi@users.noreply.github.com>
2023-10-30 22:36:52 -0500

    Fix context after merging

--- modules/pc_storage.py
+++ modules/pc_storage.py
@@ -1,8 +1,8 @@
 import math
 
 from modules.console import console
+from modules.context import context
 from modules.game import get_symbol
-from modules.gui import get_emulator, get_rom, set_message
 from modules.memory import read_symbol, unpack_uint32
 from modules.pokemon import Pokemon
 
@@ -14,7 +14,7 @@ IN_BOX_COUNT = IN_BOX_ROWS * IN_BOX_COLUMNS
 
 
 def _find_pokemon_storage_offset() -> tuple[int, int]:
-    if get_rom().game_title in ["POKEMON EMER", "POKEMON FIRE", "POKEMON LEAF"]:
+    if context.rom.game_title in ["POKEMON EMER", "POKEMON FIRE", "POKEMON LEAF"]:
         offset = unpack_uint32(read_symbol("gPokemonStoragePtr"))
         length = get_symbol("gPokemonStorage")[1]
     else:
@@ -33,7 +33,7 @@ def import_into_storage(data: bytes) -> bool:
         # first mon is stored at offset gPokemonStorage + 4
         offset_to_check = 4 + i * 80
         # if a spot is space_available, it is all 0
-        space_available = unpack_uint32(get_emulator().read_bytes(g_pokemon_storage + offset_to_check, 4)) == 0
+        space_available = unpack_uint32(context.emulator.read_bytes(g_pokemon_storage + offset_to_check, 4)) == 0
         if space_available:
             available_offset = offset_to_check
             break
@@ -42,12 +42,12 @@ def import_into_storage(data: bytes) -> bool:
     if pokemon.is_valid and space_available:
         box = math.floor(((available_offset / 80) / IN_BOX_COUNT) + 1)
         message = f"Saved {pokemon.species.name} to PC box {box}!"
-        get_emulator().write_bytes(g_pokemon_storage + available_offset, data)
-        set_message(message)
+        context.emulator.write_bytes(g_pokemon_storage + available_offset, data)
+        context.message = message
         console.print(message)
     else:
         message = f"Not enough room in PC to automatically import {pokemon.species.name}!"
-        set_message(message)
+        context.message = message
         console.print(message)
         return False
     return True
