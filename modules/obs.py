69db8887e0291ac9fcfcd9a66174bb7240e16538
40 Cakes <16377135+40Cakes@users.noreply.github.com>
2023-11-05 03:08:24 +1100

    Stop hard crash if OBS webhook fails

--- modules/obs.py
+++ modules/obs.py
@@ -6,12 +6,15 @@ from modules.config import config
 def obs_hot_key(
     obs_key: str, pressCtrl: bool = False, pressShift: bool = False, pressAlt: bool = False, pressCmd: bool = False
 ):
-    with obs.ReqClient(
-        host=config["obs"]["obs_websocket"]["host"],
-        port=config["obs"]["obs_websocket"]["port"],
-        password=config["obs"]["obs_websocket"]["password"],
-        timeout=5,
-    ) as client:
-        client.trigger_hot_key_by_key_sequence(
-            obs_key, pressCtrl=pressCtrl, pressShift=pressShift, pressAlt=pressAlt, pressCmd=pressCmd
-        )
+    try:
+        with obs.ReqClient(
+            host=config["obs"]["obs_websocket"]["host"],
+            port=config["obs"]["obs_websocket"]["port"],
+            password=config["obs"]["obs_websocket"]["password"],
+            timeout=5,
+        ) as client:
+            client.trigger_hot_key_by_key_sequence(
+                obs_key, pressCtrl=pressCtrl, pressShift=pressShift, pressAlt=pressAlt, pressCmd=pressCmd
+            )
+    except:
+        pass
