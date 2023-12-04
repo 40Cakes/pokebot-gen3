0dc53ddb1302f04fd34ac88cccea4010354545d6
Vítor <vitor.silverio.rodrigues@gmail.com>
2023-12-04 20:44:43 -0300

    Add functions to pathfinding in navaigation module

--- modules/navigation.py
+++ modules/navigation.py
@@ -1,11 +1,28 @@
+from math import sqrt
+
 from modules.context import context
 from modules.encounter import encounter_pokemon
+from modules.map import MapLocation
 from modules.memory import get_game_state, GameState
 from modules.pokemon import opponent_changed, get_opponent
 from modules.tasks import task_is_active
 from modules.temp import temp_run_from_battle
 from modules.player import get_player, TileTransitionState
 
+class Node():
+    """A node class for A* Pathfinding"""
+
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+    
 
 def follow_path(coords: list, run: bool = True) -> bool:  # TODO needs a rework
     """
@@ -63,3 +80,133 @@ def follow_path(coords: list, run: bool = True) -> bool:  # TODO needs a rework
         context.emulator.run_single_frame()  # TODO bad (needs to be refactored so main loop advances frame)
 
     return True
+
+def get_adjacents_tiles(position: tuple[int, int], current_map: MapLocation) -> list[MapLocation]:
+    """
+        Get all adjacent tile from a position that can be walkable
+        TODO Consider surf, bike, objects in the future
+    """
+    x, y = position
+    map_width, map_height = current_map.map_size
+    adjacent_tiles = []
+    
+    def valid_position(x1, y1) -> bool:
+        return x1 >= 0 and y1 >= 0 and x1 < map_width and y1 < map_height
+    
+    tiles = current_map.all_tiles
+
+    #North
+    if valid_position(x, y-1) and not tiles[x][y-1].collision:
+        adjacent_tiles.append(tiles[x][y-1])
+    
+    # South
+    if valid_position(x, y+1) and not tiles[x][y+1].collision:
+        adjacent_tiles.append(tiles[x][y+1])
+
+    # West
+    if valid_position(x-1, y) and not tiles[x-1][y].collision:
+        adjacent_tiles.append(tiles[x-1][y])
+
+    # East
+    if valid_position(x+1, y) and not tiles[x+1][y].collision:
+        adjacent_tiles.append(tiles[x+1][y])
+
+
+    return adjacent_tiles
+
+def calc_heuristic_cost(goal: tuple[int, int], tile: tuple[int, int]) -> float:
+    """
+        Heurist cost calculated with cartesian distance formula
+    """
+    xG, yG = goal
+    xP, yP = tile
+    return sqrt(((xG - xP)**2) + ((yG - yP)**2))
+
+def calc_cost(tile: tuple[int, int], current_map: MapLocation) -> float:
+    """
+        Calculate cost by tile type
+    """
+    x, y = tile
+
+    if current_map.all_tiles[x][y].is_running_possible:
+        return 0.5
+    if current_map.all_tiles[x][y].has_encounters:
+        return 1.5
+    if current_map.all_tiles[x][y].is_surfable:
+        return 2
+    
+    return 1
+
+
+def pathfinding(start: tuple[int, int], end: tuple[int, int], current_map: MapLocation) -> list[tuple[int, int]]:
+
+    # Create start and end node
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
+
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Add the start node
+    open_list.append(start_node)
+
+    # Loop until you find the end
+    while len(open_list) > 0:
+
+        # Get the current node
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        # Pop current off open list, add to closed list
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        # Found the goal
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1] # Return reversed path
+
+        # Generate children
+        children = []
+        for tile in get_adjacents_tiles(current_node.position, current_map): # Adjacent squares
+
+            # Get node position
+            node_position = tile.local_position
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+
+            # Child is on the closed list
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + calc_cost(child.position, current_map)
+            child.h = calc_heuristic_cost(goal=end, tile=child.position)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            # Add the child to the open list
+            open_list.append(child)
