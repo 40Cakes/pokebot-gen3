c1c90a4514490e3b9c3f36612c611b499a0d71c1
40 Cakes <16377135+40Cakes@users.noreply.github.com>
2023-12-19 23:26:34 +1100

    Randomise icon of notifications + add profile name to application name if system groups notifications

--- modules/gui/desktop_notification.py
+++ modules/gui/desktop_notification.py
@@ -1,13 +1,20 @@
 from pathlib import Path
 from notifypy import Notify
 
+from modules.context import context
 from modules.sprites import choose_random_sprite
 from modules.version import pokebot_name, pokebot_version
 
 
-def desktop_notification(title: str, message: str, icon: Path = choose_random_sprite()) -> None:
-    notification = Notify(default_notification_application_name=f"{pokebot_name} {pokebot_version}")
+def desktop_notification(title: str, message: str, icon: Path = None) -> None:
+    if not icon:
+        icon = choose_random_sprite()
+
+    notification = Notify(
+        default_notification_application_name=f"{context.profile.path.name} | {pokebot_name} {pokebot_version}"
+    )
     notification.title = title
     notification.message = message
     notification.icon = icon
+
     notification.send()
