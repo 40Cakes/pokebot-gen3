7798d13147bbb8f66c94e6468c9b9ad961e1e9de
40 Cakes <16377135+40Cakes@users.noreply.github.com>
2023-12-21 01:19:35 +1100

    Except new notifications system crashing bot

--- modules/gui/desktop_notification.py
+++ modules/gui/desktop_notification.py
@@ -1,20 +1,25 @@
 from pathlib import Path
 from notifypy import Notify
 
+from modules.console import console
 from modules.context import context
 from modules.sprites import choose_random_sprite
 from modules.version import pokebot_name, pokebot_version
 
 
 def desktop_notification(title: str, message: str, icon: Path = None) -> None:
-    if not icon:
-        icon = choose_random_sprite()
+    try:
+        if not icon:
+            icon = choose_random_sprite()
 
-    notification = Notify(
-        default_notification_application_name=f"{context.profile.path.name} | {pokebot_name} {pokebot_version}"
-    )
-    notification.title = title
-    notification.message = message
-    notification.icon = icon
+        notification = Notify(
+            default_notification_application_name=f"{context.profile.path.name} | {pokebot_name} {pokebot_version}"
+        )
+        notification.title = title
+        notification.message = message
+        notification.icon = icon
 
-    notification.send()
+        notification.send()
+    except:
+        console.print_exception()
+        pass
