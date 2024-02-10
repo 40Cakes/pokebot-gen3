f71d1cf6ad15bd481faeab9fb9efa5a4fef979f9
Tino <github@hanzi.cc>
2024-02-10 08:30:02 -0500

    Fix linter issues

--- modules/keyboard.py
+++ modules/keyboard.py
@@ -7,16 +7,17 @@ import json
 
 DATA_DIRECTORY = Path(__file__).parent / "data"
 
-with open(f'{DATA_DIRECTORY}/keyboard.json', 'r', encoding='utf-8') as f:
+with open(f"{DATA_DIRECTORY}/keyboard.json", "r", encoding="utf-8") as f:
     key_layout = json.load(f)
 
-#Will have to add more languages later and detect it
+# Will have to add more languages later and detect it
 lang = "en"
 
 valid_characters = []
 for num, page in enumerate(key_layout[lang]):
     for row in page["array"]:
         valid_characters.extend(row)
+
 
 class Keyboard:
     @property
@@ -27,27 +28,33 @@ class Keyboard:
             return True
         else:
             return False
-    
+
     @property
     def text_buffer(self) -> str:
         try:
             if context.rom.game_title not in ["POKEMON RUBY", "POKEMON SAPP"]:
-                return decode_string(context.emulator.read_bytes(unpack_uint32(read_symbol("sNamingScreen")) + 0x1800, 16))
+                return decode_string(
+                    context.emulator.read_bytes(unpack_uint32(read_symbol("sNamingScreen")) + 0x1800, 16)
+                )
             else:
-                return decode_string(context.emulator.read_bytes(unpack_uint32(read_symbol("namingScreenDataPtr")) + 0x11, 16))
+                return decode_string(
+                    context.emulator.read_bytes(unpack_uint32(read_symbol("namingScreenDataPtr")) + 0x11, 16)
+                )
         except Exception:
             return None
-        
+
     @property
     def cur_page(self) -> int:
         try:
             if context.rom.game_title not in ["POKEMON RUBY", "POKEMON SAPP"]:
-                return [1,2,0].index(context.emulator.read_bytes(unpack_uint32(read_symbol("sNamingScreen")) + 0x1E22, 1)[0])
+                return [1, 2, 0].index(
+                    context.emulator.read_bytes(unpack_uint32(read_symbol("sNamingScreen")) + 0x1E22, 1)[0]
+                )
             else:
-                return [0x3c,0x42,0x3F].index(context.emulator.read_bytes(0x03001858, 1)[0])
+                return [0x3C, 0x42, 0x3F].index(context.emulator.read_bytes(0x03001858, 1)[0])
         except Exception:
             return None
-    
+
     @property
     def cur_pos(self) -> tuple:
         x_val = None
@@ -55,19 +62,24 @@ class Keyboard:
         if context.rom.game_title not in ["POKEMON RUBY", "POKEMON SAPP"]:
             x_val = int(context.emulator.read_bytes(0x03007D98, 1)[0])
             if context.rom.game_title == "POKEMON EMER":
-                y_val = int(context.emulator.read_bytes(0x030023A8, 1)[0]/16)-5
+                y_val = int(context.emulator.read_bytes(0x030023A8, 1)[0] / 16) - 5
             else:
-                y_val = int(context.emulator.read_bytes(0x030031D8, 1)[0]/16)-5
+                y_val = int(context.emulator.read_bytes(0x030031D8, 1)[0] / 16) - 5
         else:
             try:
                 if self.cur_page == 2:
-                    x_val = [0x1B,0x33,0x4B,0x63,0x7B,0x93,0xBC].index(context.emulator.read_bytes(0x0300185E, 1)[0])
+                    x_val = [0x1B, 0x33, 0x4B, 0x63, 0x7B, 0x93, 0xBC].index(
+                        context.emulator.read_bytes(0x0300185E, 1)[0]
+                    )
                 else:
-                    x_val = [0x1B,0x2B,0x3B,0x53,0x63,0x73,0x83,0x9B,0xBC].index(context.emulator.read_bytes(0x0300185E, 1)[0])
+                    x_val = [0x1B, 0x2B, 0x3B, 0x53, 0x63, 0x73, 0x83, 0x9B, 0xBC].index(
+                        context.emulator.read_bytes(0x0300185E, 1)[0]
+                    )
             except Exception:
                 pass
-            y_val = int(context.emulator.read_bytes(0x0300185C, 1)[0]/16)-4
+            y_val = int(context.emulator.read_bytes(0x0300185C, 1)[0] / 16) - 4
         return (x_val, y_val)
+
 
 class BaseMenuNavigator:
     def __init__(self, step: str = "None"):
@@ -99,16 +111,17 @@ class BaseMenuNavigator:
         """
         ...
 
+
 class KeyboardNavigator(BaseMenuNavigator):
-    def __init__(self, name : str, max_length : int = 8):
+    def __init__(self, name: str, max_length: int = 8):
         super().__init__()
         if len(name) > max_length:
             name = name[:max_length]
-        self.name = ''.join([char if char in valid_characters else " " for char in name])
+        self.name = "".join([char if char in valid_characters else " " for char in name])
         self.h = key_layout[lang][0]["height"]
         self.w = key_layout[lang][0]["width"]
         self.keyboard = Keyboard()
-        
+
     def get_next_func(self):
         match self.current_step:
             case "None":
@@ -123,7 +136,7 @@ class KeyboardNavigator(BaseMenuNavigator):
                 self.current_step = "Confirm name"
             case "Confirm name":
                 self.current_step = "exit"
-                
+
     def update_navigator(self):
         match self.current_step:
             case "Check keyboard status":
@@ -136,24 +149,26 @@ class KeyboardNavigator(BaseMenuNavigator):
                 self.navigator = self.release_keys()
             case "Confirm name":
                 self.navigator = self.confirm_name()
-    
+
     def check_keyboard_status(self):
         if not self.keyboard.enabled:
             self.current_step = "exit"
             context.message = "Keyboard is not open"
-    
+
     def wait_for_keyboard(self):
-        while (self.keyboard.cur_pos[0] > self.w and self.keyboard.cur_pos[1] > self.h) or len(self.keyboard.text_buffer) > 0:
+        while (self.keyboard.cur_pos[0] > self.w and self.keyboard.cur_pos[1] > self.h) or len(
+            self.keyboard.text_buffer
+        ) > 0:
             context.emulator.press_button("B")
             yield
-        
+
     def navigate_keyboard(self):
         press = None
         last_pos = None
         cur_char = 0
-        goto = [0,0,0]
+        goto = [0, 0, 0]
         for page in range(len(key_layout[lang])):
-            for num, row  in enumerate(key_layout[lang][page]["array"]):
+            for num, row in enumerate(key_layout[lang][page]["array"]):
                 if self.name[0] in row:
                     goto = [row.index(self.name[0]), num, page]
                     break
@@ -165,13 +180,13 @@ class KeyboardNavigator(BaseMenuNavigator):
                     self.w = key_layout[lang][page]["width"]
                 spot = self.keyboard.cur_pos
                 if spot == last_pos or (last_pos is None and spot[0] <= self.w and spot[1] <= self.h):
-                    if page != goto[2]: # Press Select until on correct page
+                    if page != goto[2]:  # Press Select until on correct page
                         while page != goto[2]:
                             context.emulator.press_button("Select")
                             yield
                             page = self.keyboard.cur_page
                         last_pos = None
-                    elif spot[0] == goto[0] and spot[1] == goto[1]: # Press A if on correct character
+                    elif spot[0] == goto[0] and spot[1] == goto[1]:  # Press A if on correct character
                         last_pos = spot
                         while len(self.keyboard.text_buffer) < cur_char + 1:
                             context.emulator.press_button("A")
@@ -194,24 +209,27 @@ class KeyboardNavigator(BaseMenuNavigator):
                                             found = True
                                             break
                                     if found:
-                                        break   
+                                        break
                     else:
                         if spot[0] < goto[0]:
                             press = "Right"
                         elif spot[0] > goto[0]:
                             press = "Left"
-                        elif (spot[1] < goto[1] or (spot[1] == self.h - 1 and goto[1] == 0)) and not (spot[1] == 0 and goto[1] == self.h-1):
+                        elif (spot[1] < goto[1] or (spot[1] == self.h - 1 and goto[1] == 0)) and not (
+                            spot[1] == 0 and goto[1] == self.h - 1
+                        ):
                             press = "Down"
                         else:
                             press = "Up"
                         context.emulator.press_button(press)
+                        yield
                         yield
                         last_pos = None
                 else:
                     yield
             else:
                 yield
-    
+
     def release_keys(self):
         context.emulator.release_button("A")
         context.emulator.release_button("Down")
@@ -221,13 +239,14 @@ class KeyboardNavigator(BaseMenuNavigator):
         context.emulator.release_button("Start")
         context.emulator.release_button("Select")
         yield
-        
+
     def confirm_name(self):
         while self.keyboard.enabled:
-            if self.keyboard.cur_pos[0] > self.w or (self.keyboard.cur_pos == (8,0) and context.rom.game_title in ["POKEMON RUBY", "POKEMON SAPP"]):
+            if self.keyboard.cur_pos[0] > self.w or (
+                self.keyboard.cur_pos == (8, 0) and context.rom.game_title in ["POKEMON RUBY", "POKEMON SAPP"]
+            ):
                 context.emulator.press_button("A")
                 yield
             else:
                 context.emulator.press_button("Start")
                 yield
-    
