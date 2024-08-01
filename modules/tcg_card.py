b0fe716bf2155c0b97dab025e8eae232a175d049
wutkaru <43336971+wutkaru@users.noreply.github.com>
2024-08-02 02:24:48 +0700

    fix: tcg card can't gen when move type is ??? (#361)

--- modules/tcg_card.py
+++ modules/tcg_card.py
@@ -138,6 +138,7 @@ def generate_tcg_card(pokemon: Pokemon, location: str = "") -> Path | None:
             # Moves
             for i, move in enumerate(pokemon.moves):
                 if move:
+                    move_type_name = "Unknown" if move.move.type.name == "???" else move.move.type.name
                     move_name = "Unknown" if move.move.name == "???" else move.move.name
                     move_power = "-" if move.move.base_power == 0 else str(move.move.base_power)
                     if move_name == "Hidden Power":
@@ -162,7 +163,7 @@ def generate_tcg_card(pokemon: Pokemon, location: str = "") -> Path | None:
                         shadow_colour="#000",
                         anchor="rm",
                     )
-                    move_type = Image.open(get_sprites_path() / "types" / "swsh" / f"{move.move.type}.png")
+                    move_type = Image.open(get_sprites_path() / "types" / "swsh" / f"{move_type_name}.png")
                     if move.move.name == "Hidden Power":
                         move_type = Image.open(
                             get_sprites_path() / "types" / "swsh" / f"{pokemon.hidden_power_type}.png"
