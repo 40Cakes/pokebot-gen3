a2766e32a2ef40d12a46435cc472fb7f219acd0b
Neka <lassiazt.info@gmail.com>
2024-10-30 19:41:11 +0100

    Exclude safari map from regular spind and fishing modes

--- modules/modes/fishing.py
+++ modules/modes/fishing.py
@@ -4,6 +4,7 @@ from modules.gui.multi_select_window import Selection, ask_for_choice
 from modules.items import get_item_bag, get_item_by_name
 from modules.player import get_player, get_player_avatar
 from modules.runtime import get_sprites_path
+from modules.map_data import is_safari_map
 from ._asserts import assert_item_exists_in_bag
 from ._interface import BotMode
 from .util import fish, register_key_item
@@ -18,7 +19,7 @@ class FishingMode(BotMode):
     def is_selectable() -> bool:
         player = get_player_avatar()
         targeted_tile = player.map_location_in_front
-        return targeted_tile is not None and targeted_tile.is_surfable
+        return targeted_tile is not None and targeted_tile.is_surfable and not is_safari_map()
 
     def run(self) -> Generator:
         # Ask player to register a rod if they have one
--- modules/modes/spin.py
+++ modules/modes/spin.py
@@ -3,6 +3,7 @@ from typing import Generator
 from modules.player import get_player_avatar
 from ._interface import BotMode
 from .util import apply_white_flute_if_available, spin
+from modules.map_data import is_safari_map
 
 
 class SpinMode(BotMode):
@@ -12,7 +13,7 @@ class SpinMode(BotMode):
 
     @staticmethod
     def is_selectable() -> bool:
-        return get_player_avatar().map_location.has_encounters
+        return get_player_avatar().map_location.has_encounters and not is_safari_map()
 
     def run(self) -> Generator:
         yield from apply_white_flute_if_available()
