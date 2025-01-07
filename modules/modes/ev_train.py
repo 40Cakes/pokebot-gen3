f9a3ac32f2bb21dcd1f0e0a40ed5f2c2ab0fbdf1
Tino <1106400+hanzi@users.noreply.github.com>
2025-01-06 19:14:00 -0500

    EV Train: Improve battle strategy so it never rotates the lead
    This updates the EV Train mode's battle strategy so it always tries to run away if the lead Pokémon cannot battle.
    
    That should avoid adding unwanted EVs to other party members if the user forgot to update their `battle.yml`.

--- modules/modes/ev_train.py
+++ modules/modes/ev_train.py
@@ -1,4 +1,4 @@
-from typing import Generator
+from typing import Generator, Optional, TYPE_CHECKING
 
 from rich.table import Table
 
@@ -12,11 +12,14 @@ from modules.pokemon_party import get_party
 from ._interface import BotMode, BotModeError
 from .util import navigate_to, heal_in_pokemon_center, spin
 from .util.map import map_has_pokemon_center_nearby, find_closest_pokemon_center
-from ..battle_state import BattleOutcome
-from ..battle_strategies import BattleStrategy, DefaultBattleStrategy
+from ..battle_state import BattleOutcome, BattleState
+from ..battle_strategies import BattleStrategy, DefaultBattleStrategy, BattleStrategyUtil
 from ..console import console
 from ..encounter import handle_encounter, EncounterInfo
 from ..gui.ev_selection_window import ask_for_ev_targets
+
+if TYPE_CHECKING:
+    from modules.battle_strategies import TurnAction
 
 _list_of_stats = ("hp", "attack", "defence", "special_attack", "special_defence", "speed")
 
@@ -80,6 +83,14 @@ def _print_target_table(pokemon: Pokemon, target_evs: StatsValues) -> None:
 class NoRotateLeadDefaultBattleStrategy(DefaultBattleStrategy):
     def choose_new_lead_after_battle(self) -> int | None:
         return None
+
+    def _handle_lead_cannot_battle(
+        self, battle_state: BattleState, util: BattleStrategyUtil, reason: str
+    ) -> tuple["TurnAction", Optional[any]]:
+        # Never try to rotate into another Pokémon as that might mess up their EVs.
+        # Instead, always attempt to escape if the lead Pokémon is no longer able to
+        # battle.
+        return super()._handle_flee(battle_state, util, reason)
 
 
 class EVTrainMode(BotMode):
