517090d945d5302e09da9309467741fed42e3476
Tino <1106400+hanzi@users.noreply.github.com>
2025-01-14 06:53:03 -0500

    Add remaining field moves to `use_field_move()` function
    This adds the remaining moves that can be used in the overworld to `use_party_hm_move()` and renames that function to `use_field_move()`.
    
    Sweet Scent mode now also uses that function, which makes it _very_ simple.

--- modules/modes/sweet_scent.py
+++ modules/modes/sweet_scent.py
@@ -1,12 +1,9 @@
 from typing import Generator
 
 from modules.battle_state import BattleOutcome
-from modules.context import context
-from modules.menu_parsers import CursorOptionEmerald, CursorOptionFRLG, CursorOptionRS
-from modules.menuing import PokemonPartyMenuNavigator, StartMenuNavigator
+from modules.menuing import use_field_move
 from modules.player import get_player_avatar
-from modules.pokemon_party import get_party
-from ._asserts import assert_player_has_poke_balls, assert_has_pokemon_with_any_move
+from ._asserts import assert_player_has_poke_balls
 from ._interface import BotMode
 
 
@@ -25,21 +22,4 @@ class SweetScentMode(BotMode):
 
     def run(self) -> Generator:
         assert_player_has_poke_balls()
-
-        assert_has_pokemon_with_any_move(
-            ["Sweet Scent"], "None of your party Pokémon know the move Sweet Scent. Please teach it to someone."
-        )
-
-        yield from StartMenuNavigator("POKEMON").step()
-
-        move_pokemon = get_party().first_pokemon_with_move("Sweet Scent")
-
-        cursor = None
-        if context.rom.is_emerald:
-            cursor = CursorOptionEmerald
-        elif context.rom.is_rs:
-            cursor = CursorOptionRS
-        elif context.rom.is_frlg:
-            cursor = CursorOptionFRLG
-
-        yield from PokemonPartyMenuNavigator(move_pokemon.index, "", cursor.SWEET_SCENT).step()
+        yield from use_field_move("Sweet Scent")
