3c4d5ea2c553ed5041440ab49a2b863fd3809bec
Tino <1106400+hanzi@users.noreply.github.com>
2026-04-25 17:31:10 +0200

    Overlay: Add water bubble effect on underwater routes
    As a little easter egg, this adds an effect that shows a burst of water bubbles every now and then while the bot is hunting underwater.
    
    It has a 1/600 chance each seconds, so should only occur once every 10 minutes on average.

--- modules/web/static/stream-overlay/content/effects.js
+++ modules/web/static/stream-overlay/content/effects.js
@@ -25,4 +25,32 @@ function fireConfetti(booms, durationInSections) {
     }
 }
 
-export {fireConfetti};
+function fireWaterBubbleBurst() {
+    const spawnBubbles = (n) => {
+        const viewpointWidth = window.innerWidth;
+
+        let elements = [];
+        for (let index = 0; index < n; index++) {
+            const element = document.createElement("div");
+            element.className = "water-bubble";
+
+            const width = Math.round(Math.random() * 7 + 3);
+            const diameter = width * 7 + 2;
+            element.style.fontSize = `${width}px`;
+            element.style.left = Math.round(Math.random() * viewpointWidth - diameter / 2) + "px";
+            element.style.bottom = `-${diameter + Math.round(Math.random() * 500)}px`;
+            element.style.animationDuration = `${Math.round(Math.random() * 1000 + 500)}ms`;
+
+            document.body.appendChild(element);
+            elements.push(element);
+        }
+
+        window.setTimeout(() => elements.forEach(element => document.body.removeChild(element)), 2500);
+    }
+
+    window.setTimeout(() => spawnBubbles(35), 100);
+    window.setTimeout(() => spawnBubbles(45), 250);
+    window.setTimeout(() => spawnBubbles(55), 500);
+}
+
+export {fireConfetti, fireWaterBubbleBurst};
